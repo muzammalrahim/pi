5d5efd7cd7ca84c8fd1682f55289a6e4ae15aa8b
Shah E Room Wali <shaheroumwali@gmail.com>
2021-08-16 09:41:25 +0200

    update changes

--- pidjango/xx/templates/has_group.py
+++ pidjango/xx/templates/has_group.py
@@ -7,8 +7,9 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
+    # group = Xuser.objects.get(role=group_name)
     try:
-        group = Xuser.objects.get(name=group_name)
+        group = Xuser.objects.get(role=group_name)
     except Xuser.DoesNotExist:
         return False
     return group
