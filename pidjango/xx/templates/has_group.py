818da9f3c29ac631a9a52645386556877daaf9bd
Shah E Room Wali <shaheroumwali@gmail.com>
2021-08-17 14:17:36 +0200

    update changes

--- pidjango/xx/templates/has_group.py
+++ pidjango/xx/templates/has_group.py
@@ -9,8 +9,10 @@ def has_group(user, group_name):
     group = Xuser.objects.filter(email=user.email).first()
     if group is not None:
         if group.role == group_name:
+            print("hellllo")
             return True
         else:
+            print("sdkdskdjskdjskdj")
             return False
     else:
         return group
